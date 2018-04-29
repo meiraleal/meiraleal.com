7863c50a44f2a25d4208cfdea98e219a39fcd814
Alan Leal <alanmeira@gmail.com>
2018-04-29 20:42:54 -0300

    add Moip labs project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,78 +1,8 @@
 const projects = [
     {
-        className: 'porto-seguro',
-        description: 'Porto Seguro\'s sales platform for the entire insurance & car rental salesforce.',
-        links: {
-            website: 'https://corretor.portoseguro.com.br/corretoronline/',
-        },
-        slug: 'porto-seguro',
-        title: 'Porto Seguro',
-        cover: require('./porto-seguro.png'),
-        theme: {
-            background: '#00a5d1',
-        },
-    },
-    {
-        className: 'adnexio',
-        description: 'Web platform to allow ad managers monitor and control their campaigns, ads and budget.',
-        links: {
-            website: 'https://adnexio.com',
-        },
-        slug: 'adnexio',
-        title: 'Adnexio',
-        cover: require('./adnexio.png'),
-        theme: {
-            background: '#F7F7F7',
-            light: true,
-        },
-    },
-    {
-        className: 'engarte',
-        description: 'A marketplace for Engineers and engineering jobs.',
-        //clientDescription: '',
-        links: {
-            website: 'https://www.engarte.com',
-        },
-        slug: 'engarte',
-        title: 'Engarte',
-        cover: require('./engarte.png'),
-        theme: {
-            background: '#33414b',
-        },
-    },
-    {
-        className: 'textCorner',
-        description: 'Functional MVP of a marketplace for content writers. With it, the company got accepted to the now famous Startup Chile.',
-
-        links: {
-            website: 'https://web.archive.org/web/20130501054159/http://www.textcorner.com.br:80/',
-        },
-        slug: 'text-corner',
-        title: 'TextCorner',
-        cover: require('./text-corner.png'),
-        theme: {
-            background: '#f4f5fa',
-            light: true,
-        },
-    },
-    {
-        className: 'via6',
-        description: 'Via6 was the first business social network of Brazil, appearing around the same time Linkedin started in the US.',
-
-        links: {
-            website: 'https://www.via6.com',
-        },
-        slug: 'via6',
-        title: 'Via6',
-        cover: require('./via6.jpg'),
-        theme: {
-            background: '#e0e0e0',
-            light: true
-        },
-    },
-    {
         className: 'meSenger',
         description: 'A 1-person chatbot platform built with React Native and ClojureScript. A personal project to challenge and inspire oneself.',
+        clientDescription: '',
         links: {
             website: 'http://github.com/meiraleal/me-senger',
         },
@@ -87,6 +17,7 @@ const projects = [
     {
         className: 'alipioDigital',
         description: 'Local guide app made with React Native and ClojureScript.',
+        clientDescription: '',
         links: {
             android: 'https://play.google.com/store/apps/details?id=com.alipiodigital&hl=en',
             website: 'https://web.archive.org/web/20161108163343/http://www.alipiodigital.com:80/'
@@ -96,6 +27,94 @@ const projects = [
         cover: require('./alipio-digital.png'),
         theme: {
             background: '#b63a5c',
+        },
+    },
+    {
+        className: 'porto-seguro',
+        description: 'Porto Seguro\'s sales platform for the entire insurance & car rental salesforce.',
+        clientDescription: '',
+        links: {
+            website: 'https://corretor.portoseguro.com.br/corretoronline/',
+        },
+        slug: 'porto-seguro',
+        title: 'Porto Seguro',
+        cover: require('./porto-seguro.png'),
+        theme: {
+            background: '#00a5d1',
+        },
+    },
+    {
+        className: 'adnexio',
+        description: 'Web platform to allow ad managers monitor and control their campaigns, ads and budget.',
+        clientDescription: '',
+        links: {
+            website: 'https://adnexio.com',
+        },
+        slug: 'adnexio',
+        title: 'Adnexio',
+        cover: require('./adnexio.png'),
+        theme: {
+            background: '#F7F7F7',
+            light: true,
+        },
+    },
+    {
+        className: 'engarte',
+        description: 'A marketplace for Engineers and engineering jobs.',
+        clientDescription: '',
+        links: {
+            website: 'https://www.engarte.com',
+        },
+        slug: 'engarte',
+        title: 'Engarte',
+        cover: require('./engarte.png'),
+        theme: {
+            background: '#33414b',
+        },
+    },
+    {
+        className: 'textCorner',
+        description: 'Functional MVP of a marketplace for content writers. With it, the company got accepted to the now famous Startup Chile.',
+        clientDescription: '',
+        links: {
+            website: 'https://web.archive.org/web/20130501054159/http://www.textcorner.com.br:80/',
+        },
+        slug: 'text-corner',
+        title: 'TextCorner',
+        cover: require('./text-corner.png'),
+        theme: {
+            background: '#f4f5fa',
+            light: true,
+        },
+    },
+    {
+        className: 'via6',
+        description: 'Via6 was the first business social network of Brazil, appearing around the same time Linkedin started in the US.',
+        clientDescription: '',
+        links: {
+            website: 'https://www.via6.com',
+        },
+        slug: 'via6',
+        title: 'Via6',
+        cover: require('./via6.jpg'),
+        theme: {
+            background: '#e0e0e0',
+            light: true
+        },
+    },
+    {
+        className: 'moipLabs',
+        description: 'MoIP Labs is the open source community around the API of MoIP, one of the top brazilian digital payment gateways.',
+        clientDescription: '',
+        links: {
+            website: 'https://labs.moip.com',
+        },
+        slug: 'moip-labs',
+        title: 'Moip Labs',
+        cover: require('./via6.jpg'),
+        theme: {
+            background: '#fff',
+            light: true
         },
     },
 ];
