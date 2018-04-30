3ffdcc048423c679427606a67c45f8e6b07d08d2
Alan Leal <alanmeira@gmail.com>
2018-04-29 23:21:48 -0300

    add moip labs

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@ const projects = [
     {
         className: 'meSenger',
         description: 'A 1-person chatbot platform built with React Native and ClojureScript. A personal project to challenge and inspire oneself.',
-        clientDescription: '',
+        clientDescription: 'Me-senger is an open source side-project of mine that I use to test some concepts and new technologies like Chatbots, AI, ClojureScript, Re-frame and React Native. Its source code is available at github.',
         links: {
             website: 'http://github.com/meiraleal/me-senger',
         },
@@ -17,7 +17,7 @@ const projects = [
     {
         className: 'alipioDigital',
         description: 'Local guide app made with React Native and ClojureScript.',
-        clientDescription: '',
+        clientDescription: 'Alipio Digital was a local guide app for the neighboorhood of Alipio de Melo, Belo Horizonte. A summer project for tests with React Native. The website frontend was made using wordpress.',
         links: {
             android: 'https://play.google.com/store/apps/details?id=com.alipiodigital&hl=en',
             website: 'https://web.archive.org/web/20161108163343/http://www.alipiodigital.com:80/'
@@ -31,8 +31,8 @@ const projects = [
     },
     {
         className: 'porto-seguro',
-        description: 'Porto Seguro\'s sales platform for the entire insurance & car rental salesforce.',
-        clientDescription: '',
+        description: "Worked as a freelancer in the development of the new Porto Seguro\'s sales platform, used by their entire insurance & car rental.",
+        clientDescription: 'The development team was divided into 2 SCRUM Squads, one focused on evolution of the platform and the other for the maintenance. I worked in the frontend of the evolution squad, as a remote freelancer. The platform was developed using Meteor, React and webpack, with different small systems working together (microservices). The backend infraestructure used was Amazon AWS and the CI/CD were the Atlassian suite (Jira, bitbucket, trello & others). The time management was done using TimeDoctor.',
         links: {
             website: 'https://corretor.portoseguro.com.br/corretoronline/',
         },
@@ -46,7 +46,7 @@ const projects = [
     {
         className: 'adnexio',
         description: 'Web platform to allow ad managers monitor and control their campaigns, ads and budget.',
-        clientDescription: '',
+        clientDescription: 'Adnexio is the solution used internally by Advantage Media Group, a Danish marketing agency focused in retargeting. The product integrated Google DoubleClick, Google Adwords and other services to function as a DSP. The platform was developed using C#, Angular 2/4, RxJS and typescript. I was responsible for the Frontend and part of the devOps. The infrastructure used was Amazon AWS integrated with Github. The communication was done mainly through Skype, Slack and Github Projects',
         links: {
             website: 'https://adnexio.com',
         },
@@ -61,9 +61,9 @@ const projects = [
     {
         className: 'engarte',
         description: 'A marketplace for Engineers and engineering jobs.',
-        clientDescription: '',
+        clientDescription: 'Engarte was the leading marketplace & social network for engineers, architects and constructors around 2013. With more than 10k signed professionals and companies, the project was one of the selected for the program Startup Brasil of the year of 2014. Technologies used: Ruby on Rails, NodeJS, React. Backend: Amazon Services.',
         links: {
-            website: 'https://www.engarte.com',
+            website: 'https://web.archive.org/web/20130703025435/http://www.engarte.com:80/',
         },
         slug: 'engarte',
         title: 'Engarte',
@@ -75,7 +75,7 @@ const projects = [
     {
         className: 'textCorner',
         description: 'Functional MVP of a marketplace for content writers. With it, the company got accepted to the now famous Startup Chile.',
-        clientDescription: '',
+        clientDescription: 'Technologies used: Ruby on Rails, Bootstrap. Backend: Heroku',
         links: {
             website: 'https://web.archive.org/web/20130501054159/http://www.textcorner.com.br:80/',
         },
@@ -92,7 +92,7 @@ const projects = [
         description: 'Via6 was the first business social network of Brazil, appearing around the same time Linkedin started in the US.',
         clientDescription: '',
         links: {
-            website: 'https://www.via6.com',
+            website: 'https://web.archive.org/web/20101005100440/http://www.via6.com:80/',
         },
         slug: 'via6',
         title: 'Via6',
@@ -107,11 +107,11 @@ const projects = [
         description: 'MoIP Labs is the open source community around the API of MoIP, one of the top brazilian digital payment gateways.',
         clientDescription: '',
         links: {
-            website: 'https://labs.moip.com',
+            website: 'https://labs.moip.com/',
         },
         slug: 'moip-labs',
         title: 'Moip Labs',
-        cover: require('./via6.jpg'),
+        cover: require('./moip-labs.png'),
         theme: {
             background: '#fff',
             light: true
